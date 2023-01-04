28fb23a3cf28814678aa718d5cc85e0de6c9ce9a
eblyncho <eblyn52@naver.com>
2023-01-04 16:39:32 +0900

    클래스 컴포넌트와 함수 컴포넌트에서 state 수정하기. count up/down

--- src/App5.js
+++ src/App5.js
@@ -1,10 +1,13 @@
 import React from "react";
 import NotificationList from "./folder2/NotificationList";
-
+import CountButton from "./folder2/CountButton";
+import CountButton2 from "./folder2/CountButton2";
 function App5() {
     return (
         <div className={"container"}>
             <NotificationList />
+            <CountButton />
+            <CountButton2 />
         </div>
     );
 }
