57679ec9aaf95f98663509dbd7585fdd8e7ba31b
eblyncho <eblyn52@naver.com>
2023-01-04 16:38:19 +0900

    setState - 클래스 컴포넌트 사용

---
+++ src/App5.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NotificationList from "./folder2/NotificationList";
+
+function App5() {
+    return (
+        <div className={"container"}>
+            <NotificationList />
+        </div>
+    );
+}
+
+export default App5;
