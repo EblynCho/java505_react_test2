9b7bee22ac29744da6a0dc815ad9a9ff82ba0ecc
eblyncho <eblyn52@naver.com>
2023-01-05 18:36:59 +0900

    조건부 렌더링

---
+++ src/folder3/MadeGoal.jsx
@@ -0,0 +1,9 @@
+// MadeGoal.jsx
+
+import React from "react";
+
+function MadeGoal() {
+    return <h1>GOAL!!</h1>
+}
+
+export default MadeGoal;
---
+++ src/folder3/MissedGoal.jsx
@@ -0,0 +1,9 @@
+// MissedGoal.jsx
+
+import React from "react";
+
+function MissedGoal() {
+    return <h1>MISSED!</h1>
+}
+
+export default MissedGoal;
