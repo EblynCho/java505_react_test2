81fdf8e855a3863d5e44bdf4b615655970d363ab
eblyncho <eblyn52@naver.com>
2023-01-06 12:02:01 +0900

    Context

--- src/App7.js
+++ src/App7.js
@@ -1,10 +1,16 @@
 import React from "react";
 import Calculator from "./folder4/Calculator";
+import App from "./folder4/App";
+import App2 from "./folder4/App2";
 
 function App7(props) {
     return (
         <div className={"container"}>
-            <Calculator />
+            {/*<Calculator />*/}
+            <hr/>
+            <App />
+            <hr/>
+            <App2 />
         </div>
     );
 }
